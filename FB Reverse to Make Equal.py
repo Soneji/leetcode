9150fe3099803b45f920188af315553e9c44d8d2
Dhaval Soneji <dhaval@soneji.xyz>
2021-10-06 15:53:01 +0100

    Update FB Reverse to Make Equal.py

--- FB Reverse to Make Equal.py
+++ FB Reverse to Make Equal.py
@@ -2,13 +2,6 @@
 https://leetcode.com/discuss/interview-question/586284/facebook-interview-question-reverse-to-make-equal
 '''
 
-import math
-# Add any extra import statements you may need here
-
-
-# Add any helper functions you may need here
-
-
 def are_they_equal(array_a, array_b):
   hm = {}
   for i in array_a:
@@ -27,66 +20,3 @@ def are_they_equal(array_a, array_b):
     if hm[i] != 0:
       return False
   return True
-
-
-
-
-
-
-
-
-
-# These are the tests we use to determine if the solution is correct.
-# You can add your own at the bottom.
-def printString(string):
-  print('[\"', string, '\"]', sep='', end='')
-
-test_case_number = 1
-
-def check(expected, output):
-  global test_case_number
-  result = False
-  if expected == output:
-    result = True
-  rightTick = '\u2713'
-  wrongTick = '\u2717'
-  if result:
-    print(rightTick, 'Test #', test_case_number, sep='')
-  else:
-    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
-    printString(expected)
-    print(' Your output: ', end='')
-    printString(output)
-    print()
-  test_case_number += 1
-
-if __name__ == "__main__":
-  n_1 = 4
-  a_1 = [1, 2, 3, 4]
-  b_1 = [1, 4, 3, 2]
-  expected_1 = True
-  output_1 = are_they_equal(a_1, b_1)
-  check(expected_1, output_1)
-
-  n_2 = 4
-  a_2 = [1, 2, 3, 4]
-  b_2 = [1, 2, 3, 5]  
-  expected_2 = False
-  output_2 = are_they_equal(a_2, b_2)
-  check(expected_2, output_2)
-  
-  # n_2 = 4
-  a_2 = [1, 2, 3, 4, 4, 4]
-  b_2 = [1, 2, 3, 5, 4, 4]  
-  expected_2 = False
-  output_2 = are_they_equal(a_2, b_2)
-  check(expected_2, output_2)
-  
-  a_2 = [1, 2, 3, 4, 4, 4]
-  b_2 = [1, 4, 3, 4, 2, 4]  
-  expected_2 = True
-  output_2 = are_they_equal(a_2, b_2)
-  check(expected_2, output_2)
-
-  # Add your own test cases here
-  
